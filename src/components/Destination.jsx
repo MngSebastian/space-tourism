dd6c3b6c223c74c103b19048ef892b6b7e51938a
Seb <m.sebi97@yahoo.com>
2023-01-22 12:51:44 +0100

    Destination component Done for all devices.

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import data from "../assets/data.json"
 import { useState, useEffect } from 'react'
-
+import useMediaQuery from "../hooks/useMediaQuery"
 const Destination = () => {
   const [selectedDestination, setSelectedDestination] = useState("MOON");
   const [planetData, setPlanetData] = useState(data.destinations.find( destination => destination.name === selectedDestination))
   const [selectedIndex, setSelectedIndex] = useState(data.destinations.findIndex(destination => destination.name === "MOON"))
+  const isDesktopAndTablet = useMediaQuery("(min-width: 600px)");
+
+
+
+
   function handleClick(destination) {
     setSelectedDestination(destination)
   }
@@ -17,15 +22,32 @@ const Destination = () => {
   
   return (
     <div className='w-full h-full'>
-      <div className="flex bg-[url('./assets/destination/background-destination-mobile.jpg')] bg-no-repeat bg-cover bg-center
+      <div className="flex 
+        bg-[url('./assets/destination/background-destination-mobile.jpg')] 
+        sm:bg-[url('./assets/destination/background-destination-tablet.jpg')]
+        md:bg-[url('./assets/destination/background-destination-desktop.jpg')]
+        bg-no-repeat bg-cover bg-center
         bg-fixed  h-full w-full text-white pt-20">
         {/* TITLE DESTINATION WITH PICTURE PLANET INFO PARAGRAPH AND TRAVEL DATA */}
-        <div className='flex flex-col items-center'>
-          <h1 className='font-barlowCondensed tracking-wider text-lg'><span className='text-blue-200'>01</span> PICK YOUR DESTINATION</h1>
-          <img 
-            className='w-[180px] h-[180px] mt-4'
-            src={planetData.images.png} alt="destination"/>
-          <div className='flex font-barlowCondensed gap-8 mt-4'>
+        <div className='flex flex-col  md:w-full items-center'>
+          <h1 className='font-barlowCondensed tracking-wider md:text-[30px] sm:tracking-widest text-lg sm:pr-[60%] md:pr-[75%] sm:pl-6 sm:mt-12'>
+            <span className='text-blue-200'>01</span> PICK YOUR DESTINATION</h1>
+            {/* row div that splits screen into image and rest of data */}
+
+
+
+
+            <div className='flex flex-col md:flex-row  items-center justify-center w-full h-full'>
+              <div className='flex  justify-center  items-center md:h-5/6 md:w-2/6 md:ml-20'>
+                <img 
+                  className='xs:w-[180px] xs:h-[180px]
+                  sm:w-[280px] sm:h-[280px]
+                  md:w-[550px] md:h-[550px]
+                  mt-4'
+                  src={planetData.images.png} alt="destination"/>
+              </div>
+              <div className='flex  flex-col items-center  justify-center md:w-2/6 md:h-5/6 md:mr-20'>
+              <div className='flex font-barlowCondensed  justify-start  m: gap-8 mt-4'>
             {/* map over destinations and display each as button */}
             {data.destinations.map( (destination, index) =>{
               return (
@@ -35,21 +57,41 @@ const Destination = () => {
                     handleClick(destination.name)
                     setSelectedIndex(index)
                   }}
-                  className='tracking-widest h-[35px] focus:border-b-[3px] text-[16px] focus:text-white text-lightBlueGray'>{destination.name}</button>
+                  className='tracking-widest h-[35px] focus:border-b-[3px] text-[16px] md:text-[22px] focus:text-white text-lightBlueGray'>{destination.name}</button>
 
               )
             })}
           </div>
-          {console.log("clicked", selectedDestination)}
-          <h1 className='font-bellefair text-[52px] '>{planetData.name}</h1>
-          <p className='text-center border-b-[1px] border-gray-600 px-6 text-[16px] text-lightBlueGray mb-6 pb-4'>
+          <h1 className='font-bellefair text-[52px] sm:text-[62px] md:text-[120px] sm:mt-6'>{planetData.name}</h1>
+          <p className='text-center md:text-left  border-b-[1px] border-gray-600 px-6  w-4/6 text-[16px] md:h-[180px] md:text-[20px] text-lightBlueGray mb-6 pb-4'>
             {planetData.description}
           </p>
-          <p className='font-barlowCondensed tracking-widest text-lightBlueGray mb-1'>AVG. DISTANCE</p>
-          <h1 className='font-bellefair text-2xl tracking-wider'>{planetData.distance}</h1>
+          {isDesktopAndTablet ? (
+          <div className='flex sm:flex-row gap-24'>
+            <div>
+              <p className='font-barlowCondensed tracking-widest text-lightBlueGray mb-1'>AVG. DISTANCE</p>
+              <h1 className='font-bellefair text-2xl md:text-[30px] tracking-wider'>{planetData.distance}</h1>
+            </div>
+            <div>
+              <p className='font-barlowCondensed text-lightBlueGray tracking-widest mb-1'>EST. TRAVEL TIME</p>
+              <h1 className='font-bellefair text-2xl md:text-[30px] tracking-wider mt-1'>{planetData.travel}</h1>
+            </div>
+          </div>
 
-          <p className='font-barlowCondensed text-lightBlueGray mt-4 tracking-widest'>EST. TRAVEL TIME</p>
-          <h1 className='font-bellefair text-2xl tracking-wider mt-1'>{planetData.travel}</h1>
+          ) : (
+            <div>
+              <p className='font-barlowCondensed tracking-widest text-lightBlueGray mb-1'>AVG. DISTANCE</p>
+              <h1 className='font-bellefair text-2xl tracking-wider'>{planetData.distance}</h1>
+              <p className='font-barlowCondensed text-lightBlueGray mt-4 tracking-widest'>EST. TRAVEL TIME</p>
+              <h1 className='font-bellefair text-2xl tracking-wider mt-1'>{planetData.travel}</h1>
+            </div>
+          )}
+              </div>
+            </div>
+
+
+
+            
         </div>
       
       </div>
@@ -58,3 +100,13 @@ const Destination = () => {
 }
 
 export default Destination
+
+
+
+
+
+
+
+
+
+
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
   const isDesktopOrTablet = useMediaQuery("(min-width: 600px)");
 
   return (
-    <div className='h-full w-full'>
+    <div className='h-full w-full overflow-y-hidden'>
       <div className="flex flex-col md:flex-row justify-center items-center 
         bg-[url('./assets/home/background-home-mobile.jpg')]
         sm:bg-[url('./assets/home/background-home-tablet.jpg')]
@@ -17,7 +17,7 @@ const Landing = () => {
 
 
         {isDesktopOrTablet ? (
-          <div className='flex sm:flex-col md:flex-row items-center w-full h-full md:justify-between md:px-[20%] '>
+          <div className='flex sm:flex-col md:flex-row items-center  w-full h-full md:justify-between md:px-[20%] '>
             <div className='sm:flex sm:flex-col sm:justify-center sm:items-center '>
               <h5 className=' text-[22px] text-lightBlueGray tracking-widest font-thin font-barlowCondensed sm:pt-32 md:text-[40px]'>SO, YOU WANT TO TRAVEL TO</h5>
               <p className='font-bellefair text-[90px] sm:text-[110px] md:text-[140px] md:pl-7'>SPACE</p>
@@ -35,7 +35,7 @@ const Landing = () => {
 
           </div>
         ) : (
-          <div className='flex flex-col items-center w-full h-full md:px-[20%] '>
+          <div className='w-full h-6/6 flex flex-col items-center bg-red-500  md:px-[20%] '>
           <h5 className=' text-xl text-lightBlueGray font-thin font-barlowCondensed pt-20'>SO, YOU WANT TO TRAVEL TO</h5>
           <p className='font-bellefair text-[70px] md:pl-7'>SPACE</p>
           <p className='text-lightBlueGray leading-7 text-[15px] text-center px-7 '>Let's face it, if you want to go to space, you might as well genuinely go to outer space and
